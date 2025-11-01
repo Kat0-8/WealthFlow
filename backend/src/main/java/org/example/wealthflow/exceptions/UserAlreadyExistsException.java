fe0a20c87c71ea6dedec14e2c2ed106849f49337
Daniil Prokopovich 350504 <prokopovich-dan-bsuir@inbox.ru>
2025-11-01 16:21:57 +0300

    Renamed UserAlreadyExists -> AlreadyExists exception

--- backend/src/main/java/org/example/wealthflow/exceptions/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package org.example.wealthflow.exceptions;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
