c6eb31368585e3f83e614e03b725646bb14e08b0
Daniil Prokopovich 350504 <prokopovich-dan-bsuir@inbox.ru>
2025-11-04 17:57:00 +0300

    Entrypoint for jwt filter

---
+++ backend/src/main/java/org/example/wealthflow/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,44 @@
+package org.example.wealthflow.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.wealthflow.dtos.error.ErrorResponseDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.Instant;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger log = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        log.debug("Authentication failure: {}", authException.getMessage());
+
+        ErrorResponseDto err = ErrorResponseDto.builder()
+                .timestamp(Instant.now())
+                .status(HttpServletResponse.SC_UNAUTHORIZED)
+                .error("Unauthorized")
+                .message(authException.getMessage())
+                .path(request.getRequestURI())
+                .build();
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(objectMapper.writeValueAsString(err));
+    }
+}
