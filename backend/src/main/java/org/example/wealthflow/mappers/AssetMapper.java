24a0acc5cdbc271c0d673e822722aec483391367
Daniil Prokopovich 350504 <prokopovich-dan-bsuir@inbox.ru>
2025-11-06 23:34:50 +0300

    Cosmetic change

--- backend/src/main/java/org/example/wealthflow/mappers/AssetMapper.java
+++ backend/src/main/java/org/example/wealthflow/mappers/AssetMapper.java
@@ -9,7 +9,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public abstract class AssetMapper {
